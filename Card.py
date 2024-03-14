70ed41b74eda7c2e1f6fe0fe3759cd697197a61a
Егорова Виктория <viktori_700@mail.ru>
2024-03-14 09:51:39 +0300

    clean_images

--- Card.py
+++ Card.py
@@ -5,7 +5,7 @@ class Card:
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
-        self.im_card = pygame.image.load(f"images/cards/{self.value}_{self.suit}.png").convert_alpha()
+        self.im_card = pygame.image.load(f"images/cards_kmpo/{self.value}_{self.suit}.png").convert_alpha()
         self.im_card = pygame.transform.scale(self.im_card, (self.im_card.get_width() // 12, self.im_card.get_height() // 12))
 
     def __str__(self):
